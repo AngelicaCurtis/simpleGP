c55fefca4877b4c1bd23dd51edc700af7282f3e8
unknown <angelicacurtis.f@gmail.com>
2018-10-01 00:41:45 -0300

    atualização bd

--- simpleGP/settings.py
+++ simpleGP/settings.py
@@ -85,10 +85,17 @@ WSGI_APPLICATION = 'simpleGP.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
+# default_dburl = 'sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3')
 
 DATABASES = {
-    'default': config('DATABASE_URL', default=default_dburl, cast=dburl),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'simple-gp',
+        'USER': 'postgres',
+        'PASSWORD': 'sitrucatina',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
 }
 
 # Password validation
